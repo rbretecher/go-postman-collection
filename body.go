568a27dd1ce58c5e56cd2d22a9d1b81a71cb7e2b
Romain Bretécher <rbretecher@lafourchette.com>
2019-11-17 23:39:45 +0100

    fix: do not export unused body params

--- body.go
+++ body.go
@@ -2,10 +2,10 @@ package postman
 
 type Body struct {
 	Mode       string      `json:"mode"`
-	Raw        string      `json:"raw"`
-	URLEncoded interface{} `json:"urlencoded"`
-	FormData   interface{} `json:"formdata"`
-	File       interface{} `json:"file"`
-	GraphQL    interface{} `json:"graphql"`
-	Disabled   bool        `json:"disabled"`
+	Raw        string      `json:"raw,omitempty"`
+	URLEncoded interface{} `json:"urlencoded,omitempty"`
+	FormData   interface{} `json:"formdata,omitempty"`
+	File       interface{} `json:"file,omitempty"`
+	GraphQL    interface{} `json:"graphql,omitempty"`
+	Disabled   bool        `json:"disabled,omitempty"`
 }
