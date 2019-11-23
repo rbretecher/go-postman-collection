1c333bd559210ed0666030e2a80b23d98df93782
Romain Bretécher <rbretecher@lafourchette.com>
2019-11-24 00:36:12 +0100

    feat: add parsing example

---
+++ examples/parse/parse_collection.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rbretecher/go-postman-collection"
+)
+
+func main() {
+
+	c, err := postman.ParseCollection("examples/parse/parse_collection.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	exploreItems(c.Item)
+}
+
+func exploreItems(items []postman.Items) {
+	for _, i := range items {
+		if i.IsGroup() {
+			folder := i.(*postman.ItemGroup)
+			println(">", folder.Name)
+			exploreItems(folder.Item)
+		} else {
+			item := i.(*postman.Item)
+			println(fmt.Sprintf("[%s] - %s - %s", item.Request.Method, item.Request.URL.(map[string]interface{})["raw"], item.Name))
+		}
+	}
+}
