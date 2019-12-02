4eb90a1c70ce017bf5b0aebaee87947f291f2875
Romain Bretécher <rbretecher@lafourchette.com>
2019-12-02 18:30:14 +0100

    test: add test for url unmarshal

--- url_test.go
+++ url_test.go
@@ -1,6 +1,7 @@
 package postman
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,19 +18,19 @@ func TestURLString(t *testing.T) {
 func TestURLMarsalJSON(t *testing.T) {
 	cases := []struct {
 		scenario       string
-		url            *URL
+		url            URL
 		expectedOutput string
 	}{
 		{
 			"Successfully marshalling an URL as a string",
-			&URL{
+			URL{
 				Raw: "http://www.google.fr",
 			},
 			"\"http://www.google.fr\"",
 		},
 		{
 			"Successfully marshalling an URL with variables as a struct",
-			&URL{
+			URL{
 				Raw: "http://www.google.fr",
 				Variables: []*Variable{
 					{
@@ -46,5 +47,52 @@ func TestURLMarsalJSON(t *testing.T) {
 		bytes, _ := tc.url.MarshalJSON()
 
 		assert.Equal(t, tc.expectedOutput, string(bytes), tc.scenario)
+	}
+}
+
+func TestURLUnmarshalJSON(t *testing.T) {
+	cases := []struct {
+		scenario      string
+		bytes         []byte
+		expectedURL   URL
+		expectedError error
+	}{
+		{
+			"Successfully unmarshalling an URL as a string",
+			[]byte("\"http://www.google.fr\""),
+			URL{
+				Raw: "http://www.google.fr",
+			},
+			nil,
+		},
+		{
+			"Successfully unmarshalling an URL with variables as a struct",
+			[]byte("{\"raw\":\"http://www.google.fr\",\"variable\":[{\"name\":\"a-variable\",\"value\":\"an-awesome-value\"}]}"),
+			URL{
+				Raw: "http://www.google.fr",
+				Variables: []*Variable{
+					{
+						Name:  "a-variable",
+						Value: "an-awesome-value",
+					},
+				},
+			},
+			nil,
+		},
+		{
+			"Failed to unmarshal an URL because of an unsupported type",
+			[]byte("not-a-valid-url"),
+			URL{},
+			errors.New("Unsupported type"),
+		},
+	}
+
+	for _, tc := range cases {
+
+		var u URL
+		err := u.UnmarshalJSON(tc.bytes)
+
+		assert.Equal(t, tc.expectedURL, u, tc.scenario)
+		assert.Equal(t, tc.expectedError, err, tc.scenario)
 	}
 }
